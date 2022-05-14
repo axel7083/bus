af20200e0f247a986b30bb18766f1b7594837dbc
Galyfray <galyfray@gmail.com>
2022-05-14 14:33:18 +0200

    cleaned bus lines

--- Code/BusLine.py
+++ Code/BusLine.py
@@ -1,17 +1,45 @@
-from main import RunningBoard
+from typing import List
+
+from main import RunningBoard, Road
 
 
 class BusLine:
-    def __init__(self, name: str, path, board: RunningBoard):
+    def __init__(self, name: str, path: List[Road], board: RunningBoard):
+        """ Define a bus line
+
+        :param name: The name of the bus line
+        :param path: The path that the buses will follow. The path must loop.
+        :param board: The Running board of the line
+        """
         self.__name = name
         self.__path = path
+        if self.__path[0].get_position().get_begin_point() != self.__path[-1].get_position().get_end_point():
+            raise ValueError(
+                f"The path must loop but the end is at {str(self.__path[-1].get_position().get_end_point())}\
+                while the start is at {str(self.__path[0].get_position().get_begin_point())}"
+            )
         self.__runningBoard = board
 
-    def get_name(self):
+    def get_name(self) -> str:
+        """
+        :return: The name of the bus line
+        """
         return self.__name
 
-    def get_path(self):
-        return self.__path
+    def get_path(self) -> List[Road]:
+        """
+        :return: A copy of the path that buses follow.
+        """
+        return self.__path.copy()
 
-    def get_running_board(self):
+    def get_running_board(self) -> RunningBoard:
+        """
+        :return: The running bord of the bus line
+        """
         return self.__runningBoard
+
+    def get_punctuality(self):
+        """
+        :return:
+        """
+        pass
