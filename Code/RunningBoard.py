130bef5308e08896b7ed4b6b236aaade1313fec9
Galyfray <galyfray@gmail.com>
2022-05-14 15:21:44 +0200

    started to rework the running board

--- Code/RunningBoard.py
+++ Code/RunningBoard.py
@@ -1,6 +1,19 @@
-class RunningBoard:
-    def __init__(self, timetable):
-        self.__timetable = timetable
+from BusStop import BusStop
+from StopSchedule import StopSchedule
 
-    def get_timetable(self):
-        return self.__timetable
+
+class RunningBoard:
+    def __init__(self):
+        self.__timetable = {}
+        self.__index_map = {}
+
+    def add_entry(self, bus_stop: BusStop, schedule: StopSchedule):
+        if bus_stop in self.__timetable:
+            raise ValueError("This buss stop is already registered in this running board")
+        elif schedule.get_order() in self.__index_map:
+            raise ValueError(
+                f"A bus stop with the scheduled order {schedule.get_order()}\
+                 already exist int this running board"
+            )
+        else:
+            self.__timetable[bus_stop] = schedule
