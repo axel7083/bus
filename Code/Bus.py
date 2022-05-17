5788b52748b03be690eaf1e4ad19e1f5ace0d555
nexowo <jmaccou@outlook.fr>
2022-05-17 13:49:17 +0200

    Bus name fixed

--- Code/Bus.py
+++ Code/Bus.py
@@ -1,6 +1,3 @@
-from asyncio import wait
-
-
 class Bus :
     __speed = 0
     __passenger = 0
@@ -8,38 +5,32 @@ class Bus :
     
     def __init__(self, newCapa) -> None:
         self.__maxCapacity = newCapa
-
-    def printState(self) -> None:
-        print(self.getCapacity(), self.getPassenger(), self.getSpeed())
-
+    
     def __str__(self) -> str:
         state = 'Capacity = ' + str(self.getCapacity()) + '\nSpeed = ' + str(self.getSpeed()) + '\nPassengers = ' + str(self.getPassenger())
         return state
 
-    def getCapacity(self) -> int:
+    def get_capacity(self) -> int:
         return self.__maxCapacity
 
-    def getPassenger(self) -> int:
+    def get_passenger(self) -> int:
         return self.__passenger
 
-    def getSpeed(self) -> float:
+    def get_speed(self) -> float:
         return self.__speed
 
-    def setSpeed(self, newSpeed) -> None:
+    def set_speed(self, newSpeed) -> None:
         self.__speed = newSpeed
 
-    def addOccupent(self, nbNewOccupents):
+    def add_occupent(self, nbNewOccupents):
         if self.__passenger + nbNewOccupents <= self.__maxCapacity:
             self.__passenger += nbNewOccupents
         else:
             self.__passenger = self.__maxCapacity
 
-    def removeOccupent(self, nbOccupentsLeaving):
+    def remove_occupent(self, nbOccupentsLeaving):
         if self.__passenger - nbOccupentsLeaving >= 0:
             self.__passenger -= nbOccupentsLeaving
         else:
             self.__passenger = 0
-
-    def busStop(self, stop):
-        wait(10)
         
