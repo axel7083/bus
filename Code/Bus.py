fd2a75fae052410f20d631e927a3d96f3dc8e4af
Zine <49556588+Nexowo@users.noreply.github.com>
2022-05-17 11:25:39 +0200

    Clear implementation of the bus class

--- Code/Bus.py
+++ Code/Bus.py
@@ -1,14 +1,45 @@
-class Bus:
-    def __init__(self, speed: int, occupant: int, max_capacity: int):
-        self.__speed = speed
-        self.__occupant = occupant
-        self.__max_capacity = max_capacity
+from asyncio import wait
 
-    def get_speed(self):
+
+class Bus :
+    __speed = 0
+    __passenger = 0
+    __maxCapacity = 0
+    
+    def __init__(self, newCapa) -> None:
+        self.__maxCapacity = newCapa
+
+    def printState(self) -> None:
+        print(self.getCapacity(), self.getPassenger(), self.getSpeed())
+
+    def __str__(self) -> str:
+        state = 'Capacity = ' + str(self.getCapacity()) + '\nSpeed = ' + str(self.getSpeed()) + '\nPassengers = ' + str(self.getPassenger())
+        return state
+
+    def getCapacity(self) -> int:
+        return self.__maxCapacity
+
+    def getPassenger(self) -> int:
+        return self.__passenger
+
+    def getSpeed(self) -> float:
         return self.__speed
 
-    def get_occupant(self):
-        return self.__occupant
+    def setSpeed(self, newSpeed) -> None:
+        self.__speed = newSpeed
 
-    def get_max_capacity(self):
-        return self.__max_capacity
+    def addOccupent(self, nbNewOccupents):
+        if self.__passenger + nbNewOccupents <= self.__maxCapacity:
+            self.__passenger += nbNewOccupents
+        else:
+            self.__passenger = self.__maxCapacity
+
+    def removeOccupent(self, nbOccupentsLeaving):
+        if self.__passenger - nbOccupentsLeaving >= 0:
+            self.__passenger -= nbOccupentsLeaving
+        else:
+            self.__passenger = 0
+
+    def busStop(self, stop):
+        wait(10)
+        
