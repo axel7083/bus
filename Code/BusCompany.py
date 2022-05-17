132a453681a91c0a6fa7611c2cf6297edecbb92c
nexowo <jmaccou@outlook.fr>
2022-05-17 14:37:28 +0200

    PyDoc added

--- Code/BusCompany.py
+++ Code/BusCompany.py
@@ -1,15 +1,20 @@
+from Bus import Bus
+from BusLine import BusLine
+from typing import List
+
+
 class BusCompany:
-    def __init__(self, bus_lines, bus_list) -> None:  # def __init__(self, bus_lines: list[BusLine], bus_list: list[Bus]):
+    def __init__(self, bus_lines: List[BusLine], bus_list: List[Bus]) -> None:  # def __init__(self, bus_lines: list[BusLine], bus_list: list[Bus]):
         self.__bus_lines = bus_lines
         self.__bus_list = bus_list
 
-    def get_bus_lines(self):
+    def get_bus_lines(self) -> List[BusLine]:
         return self.__bus_lines
 
-    def get_bus_list(self):
+    def get_bus_list(self) -> List[Bus]:
         return self.__bus_list
     
-    def get_number_of_buses(self):
+    def get_number_of_buses(self) -> int:
         return self.__bus_list.len()
     
     def get_total_capacity(self) -> int:
