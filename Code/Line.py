09aaa9a30d4363981196b64dec41addf9baa5f8a
Galyfray <galyfray@gmail.com>
2022-05-18 22:17:32 +0200

    made roads able to check if a point is close to them

--- Code/Line.py
+++ Code/Line.py
@@ -1,4 +1,5 @@
-import Point2D
+from Point2D import Point2D
+from typing import Tuple
 
 
 class Line:
@@ -11,3 +12,32 @@ class Line:
 
     def get_end_point(self) -> Point2D:
         return self.__ending_point
+
+    def get_parameters(self) -> Tuple[float, float, float]:
+        """
+        Returns a tuple of the form (a, b, c) representing the line
+        ax + by + c= 0
+        """
+        a = self.__begin_point.get_y() - self.__ending_point.get_y()
+        b = self.__ending_point.get_x() - self.__begin_point.get_x()
+        c = a * self.__begin_point.get_x() + b * self.__begin_point.get_y()
+        return a, b, -c
+
+
+class LineMath:
+    @staticmethod
+    def get_perpendicular(a: float, b: float, point: Point2D):
+        """
+
+        :param a: the value of a in the equation ax + by + c= 0 which describes the line you want to find
+         the perpendicular line to
+        :param b: the value of b in the equation ax + by + c= 0 which describes the line you want to find
+        :param point: the point the perpendicular must go through
+        :return: Tuple[float,float,float] : a,b,c such that the line described by ax + by + c = 0 is the perpendicular
+         to the line described by the parameters a and b going through `point`
+        """
+        return (
+            b,
+            -a,
+            a * point.get_y() + -b * point.get_x()
+        )
