ab1c99895c8521986e8c5891484fbe15c4da8e10
nexowo <jmaccou@outlook.fr>
2022-05-20 14:14:11 +0200

    BusStop implementation

--- Code/BusStop.py
+++ Code/BusStop.py
@@ -1,15 +1,25 @@
+from random import randint
 from typing import Tuple
 from Point2D import Point2D
+import Bus
 
 """ Class that represents the bus Stops, it inherits from Point2D as we consider it as a point on the road.
 
 @Author: jmaccou
 """
 class BusStop(Point2D):
-    def __init__(self, name: str, nb_client: int, x: float, y: float):
+
+    __nbClient = 0
+
+    def __init__(self, name: str, x: float, y: float):
         super().__init__(x, y)
         self.__name = name
-        self.__nbClient = nb_client
+
+    def __eq__(self, __o: object) -> bool:
+        if isinstance(__o, BusStop):
+            return self.get_pos() == __o.get_pos() and self.__name == __o.__name
+        else:
+            return False
 
     def get_name(self) -> str:
         return self.__name
@@ -20,8 +30,7 @@ class BusStop(Point2D):
     def get_pos(self) -> Tuple[float, float]:
         return self.get_x(), self.get_y()
 
-    def __eq__(self, __o: object) -> bool:
-        if isinstance(__o, BusStop):
-            return self.get_pos() == __o.get_pos() and self.__name == __o.__name
-        else:
-            return False
+    def bus_happens(bus : Bus):
+        nbPassenger = bus.get_passenger()
+        if nbPassenger > 0:
+            bus.remove_passenger(randint(0,nbPassenger))
