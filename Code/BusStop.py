e6cea7324f639a8a5a6ebc31705085bf2ee56d8f
nexowo <jmaccou@outlook.fr>
2022-05-24 12:07:10 +0200

    BusStop implementation

--- Code/BusStop.py
+++ Code/BusStop.py
@@ -1,7 +1,7 @@
 from random import randint
 from typing import Tuple
 from Point2D import Point2D
-import Bus
+from Bus import Bus
 
 """ Class that represents the bus Stops, it inherits from Point2D as we consider it as a point on the road.
 
@@ -11,9 +11,10 @@ class BusStop(Point2D):
 
     __nbClient = 0
 
-    def __init__(self, name: str, x: float, y: float):
+    def __init__(self, name: str, attractivity: int, x: float, y: float):
         super().__init__(x, y)
         self.__name = name
+        self.__attractivity = attractivity
 
     def __eq__(self, __o: object) -> bool:
         if isinstance(__o, BusStop):
@@ -30,7 +31,24 @@ class BusStop(Point2D):
     def get_pos(self) -> Tuple[float, float]:
         return self.get_x(), self.get_y()
 
-    def bus_happens(bus : Bus):
-        nbPassenger = bus.get_passenger()
-        if nbPassenger > 0:
-            bus.remove_passenger(randint(0,nbPassenger))
+    def bus_happens(self, bus : Bus):
+        bus.remove_passenger(self.__weightedrandint(bus.get_passenger(), self.__attractivity))
+        bus.add_passenger(max(self.__nbClient, bus.get_capacity()-bus.get_passenger()))
+
+
+    @staticmethod
+    def __weightedrandint(max: int, weight : int) -> int:
+        if weight == 0:
+            return max
+        elif weight == 1 or weight == 3:
+            sum =0
+            for _ in range(int(1.6 * max)):
+                sum+=randint(0,1)
+            if sum > max:
+                sum = 2 * max - sum 
+            if weight == 1:
+                return sum
+            else:
+                return max - sum
+        elif weight == 2:
+            return randint(0, max)
